66ec1611c394749f6ee5c8c2c9a0823ca7b732ee
Gavri Fernandez <gavri.fernandez@gmail.com>
2026-03-17 19:29:39 +0800

    test: use chronological mapping schema window

--- astra/src/test/java/com/slack/astra/elasticsearchApi/ElasticsearchApiServiceTest.java
+++ astra/src/test/java/com/slack/astra/elasticsearchApi/ElasticsearchApiServiceTest.java
@@ -565,7 +565,7 @@ public class ElasticsearchApiServiceTest {
         new ElasticsearchApiService(searcher, DEFAULT_ASTRA_CONFIG);
 
     Instant start = Instant.now();
-    Instant end = start.minusSeconds(60);
+    Instant end = start.plusSeconds(60);
 
     when(searcher.getSchema(
             eq(
