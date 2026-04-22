f0f20eaf1732928de78055e8b1ae14e3ad75516b
Gavri Fernandez <gavri.fernandez@gmail.com>
2026-04-22 21:26:49 +0800

    Add shared downgrade allocator reuse test

--- astra/src/test/java/com/slack/astra/server/partitionassignment/PartitionAutoAssignerTest.java
+++ astra/src/test/java/com/slack/astra/server/partitionassignment/PartitionAutoAssignerTest.java
@@ -220,6 +220,32 @@ public class PartitionAutoAssignerTest {
   }
 
   @Test
+  public void shouldReuseCurrentDedicatedPartitionsWhenDowngradingToShared() {
+    DatasetMetadata datasetMetadata =
+        new DatasetMetadata(
+            "payments",
+            "owner",
+            100,
+            List.of(new DatasetPartitionMetadata(1, Long.MAX_VALUE, List.of("1", "2"))),
+            "",
+            true);
+
+    assertThat(
+            PartitionAutoAssigner.autoAssign(
+                datasetMetadata,
+                100,
+                false,
+                List.of(
+                    new LivePartitionState(
+                        "1", 50, 100, new PartitionOccupancy.Dedicated("payments")),
+                    new LivePartitionState(
+                        "2", 50, 100, new PartitionOccupancy.Dedicated("payments")),
+                    new LivePartitionState("3", 0, 100, new PartitionOccupancy.Empty())),
+                2))
+        .containsExactly("1", "2");
+  }
+
+  @Test
   public void shouldGrowSharedAssignmentsByKeepingCurrentPartitionsAndAddingTightestFit() {
     DatasetMetadata datasetMetadata =
         new DatasetMetadata(
