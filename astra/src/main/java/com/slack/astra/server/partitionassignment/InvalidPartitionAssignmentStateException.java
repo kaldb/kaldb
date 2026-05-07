79320be76523dc772745b8e250aa9c8b033b10f2
Gavri Fernandez <gavri.fernandez@gmail.com>
2026-05-07 16:34:46 +0700

    Document invalid assignment state exception

--- astra/src/main/java/com/slack/astra/server/partitionassignment/InvalidPartitionAssignmentStateException.java
+++ astra/src/main/java/com/slack/astra/server/partitionassignment/InvalidPartitionAssignmentStateException.java
@@ -2,6 +2,11 @@ package com.slack.astra.server.partitionassignment;
 
 /** Thrown when persisted partition assignment metadata violates required invariants. */
 public class InvalidPartitionAssignmentStateException extends RuntimeException {
+  /**
+   * Creates a new exception describing an invalid persisted partition-assignment state.
+   *
+   * @param message human-readable detail about the violated invariant
+   */
   public InvalidPartitionAssignmentStateException(String message) {
     super(message);
   }
