e2134885819b813944f88d20ea7e33699420cd70
rk <ram@kaldb.com>
2026-06-22 16:36:40 -0700

    Add comment.

--- astra/src/main/java/com/slack/astra/logstore/search/LogIndexSearcher.java
+++ astra/src/main/java/com/slack/astra/logstore/search/LogIndexSearcher.java
@@ -3,5 +3,9 @@ package com.slack.astra.logstore.search;
 import java.io.Closeable;
 
 public interface LogIndexSearcher<T> extends Closeable {
+  /**
+   * Executes a search for the supplied query, applying the query's requested sort fields when
+   * present and the deterministic internal ordering otherwise.
+   */
   SearchResult<T> search(SearchQuery query);
 }
